28903d6ba468e34867dd03c2b2172482c88c35a5
Geoff Carrier <Geoff.Carrier@fdmgroup.com>
2018-04-26 11:00:42 +0100

    First upload of web musings.

---
+++ WebSite/resources/HowDoesItWork/How_does_it_work.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20150131
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('How_does_it_work_files/How_does_it_workMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
